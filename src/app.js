4a6957da40facb05cc02e64bbf7d4ed3b0a28a52
spaceforestchu <spaceforestchu@gmail.com>
2018-02-21 23:51:34 -0500

    first commit

---
+++ src/app.js
@@ -0,0 +1,40 @@
+import express from 'express';
+import path from 'path';
+import logger from 'morgan';
+import bodyParser from 'body-parser';
+import index from './routes/index';
+
+const app = express();
+app.disable('x-powered-by');
+
+// View engine setup
+app.set('views', path.join(__dirname, '../views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev', {
+  skip: () => app.get('env') === 'test'
+}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, '../public')));
+
+// Routes
+app.use('/', index);
+
+// Catch 404 and forward to error handler
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// Error handler
+app.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
+  res
+    .status(err.status || 500)
+    .render('error', {
+      message: err.message
+    });
+});
+
+export default app;
---
+++ src/routes/index.js
@@ -0,0 +1,12 @@
+import { Router } from 'express';
+
+const routes = Router();
+
+/**
+ * GET home page
+ */
+routes.get('/', (req, res) => {
+  res.render('index');
+});
+
+export default routes;
