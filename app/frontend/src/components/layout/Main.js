89d8b7b3525562843297fc78209592a96826224c
spaceforestchu <spaceforestchu@gmail.com>
2018-02-25 12:21:00 -0500

    add sass

---
+++ app/frontend/src/components/layout/Main.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { Header } from '../containers';
+
+class Main extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <h1>Main</h1>
+      </div>
+    );
+  }
+}
+
+export default Main;
